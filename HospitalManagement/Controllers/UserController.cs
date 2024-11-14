0ba8981b844399c13476e35040b65d25798d9b54
RJ <RJ@RJ-SYSTEM>
2024-11-14 08:24:13 +0530

    Add project files.

---
+++ HospitalManagement/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using HospitalManagement.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalManagement.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HospitalManagement/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using HospitalManagement.Models.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalManagement.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) { }
+        DbSet<User>users { get; set; }
+    }
+}
